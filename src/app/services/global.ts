94b92ebc189ba4892808a7075ea86efc0903684d
Rubench0 <rubenchoo.garcia@gmail.com>
2019-08-04 18:55:14 -0400

    ruta de api

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,5 @@
 export var GLOBAL = {
 	// url : 'http://192.168.1.2/sacrepad/sacrepad-api/public/index.php'
-	url : 'http://localhost/sacrepad/sacrepad-api/public/index.php'
+	url : 'http://localhost/saprcepad/sacrepad-api/public/index.php'
+	// url : 'http://localhost/sacrepad/sacrepad-api/public/index.php'
 };
